0c640102c14be57ae0280c8bbd2888c1295860ef
Kenny Lindblom <kennylindblom93@gmail.com>
2022-11-08 21:17:24 +0100

    Skapat basklass för projektet

---
+++ Labb3-Polymorphism/Geometri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb3_Polymorphism
+{
+     class Geometri   //Parent Class 
+    {
+        virtual public double Area()
+        {
+            double area = 0;
+            return area;
+        }
+    }
+}
