4b529f1f3e41198261633cdb4a986241b3349a47
Kenny Lindblom <kennylindblom93@gmail.com>
2022-11-08 22:03:47 +0100

    Klass för Rektangel skapad

--- Labb3-Polymorphism/Program.cs
+++ Labb3-Polymorphism/Program.cs
@@ -6,13 +6,17 @@ namespace Labb3_Polymorphism
     {
         static void Main(string[] args)
         {
-            Geometri geometri;
+            Geometri geometri;  //Objekt reference
 
             geometri = new Cirkel();
             Console.WriteLine($"Area av cirklen: {geometri.Area()}");
 
             geometri = new Fyrkant();
-            Console.WriteLine($"Area av cirklen: {geometri.Area()}");
+            Console.WriteLine($"Area av Fyrkant: {geometri.Area()}");
+
+            geometri = new Rektangel();
+            Console.WriteLine($"Area av Rektangel: {geometri.Area()}");
+
 
 
         }
---
+++ Labb3-Polymorphism/Rektangel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb3_Polymorphism
+{
+     class Rektangel : Geometri
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+        public Rektangel()
+        {
+            Base = 9.01;
+            Height = 2;
+        }
+
+        override public double Area()
+        {
+            double area = Base * Height;
+            return area;
+        }
+
+    }
+}
